284ca08a9bd0bf067bfa1b26d8046f3cd44ff976
Rudi Kraeher <rudi.kraeher@pipedrive.com>
2022-06-05 12:46:51 +0200

    add Home basic components

---
+++ src/components/Home.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Title = styled.h1`
+    text-align: center;
+    font-size: 3vw;
+    margin: 3vw 0;
+`;
+
+export const Diagram = styled.img`
+    margin: 3vw;
+`;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import telegraphSchematicImage from '../images/telegraph-schematic.svg';
+import * as S from './Home.styles';
 
 export const Home = ()=> {
   return (
-    <div>
-        <h1>
-            First issue coming soon...
-        </h1>
-    </div>
+    <S.Container>
+        <S.Title>
+            Issue 1 coming soon
+        </S.Title>
+        <S.Diagram src={telegraphSchematicImage} alt="image of a telegraphic schematic" />
+    </S.Container>
   )
 };
