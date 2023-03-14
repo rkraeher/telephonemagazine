0091dd53106aa6d574c7315d814e086b4d70a928
Rudi Kraeher <rudi.kraeher@pipedrive.com>
2023-03-14 12:56:01 +0100

    create About page

---
+++ src/components/About.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+export const Header = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+`;
+
+export const Text = styled.p`
+  font-size: 1.2rem;
+  margin-bottom: 1rem;
+`;
---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import * as S from './About.styles';
+
+export const About = () => {
+  return (
+    <S.Container>
+      
+      <S.Header>About Us</S.Header>
+      
+      <S.Text>Welcome to our online literary magazine! We are a community of writers and readers who are passionate about the written word.</S.Text>
+
+      <S.Text>Our mission is to provide a platform for emerging and established writers to share their work with a wider audience. We publish fiction, poetry, essays, and more, and we welcome submissions from writers of all backgrounds and levels of experience.</S.Text>
+
+      <S.Text>Thank you for visiting our site, and we hope you enjoy reading our stories as much as we enjoy sharing them.</S.Text>
+
+    </S.Container>
+  );
+};
