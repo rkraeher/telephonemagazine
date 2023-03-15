f7bde943bdf3bc0270463179aaeb165ee07ee30f
Rudi Kraeher <rudi.kraeher@pipedrive.com>
2023-03-15 11:15:08 +0100

    add masthead page

---
+++ src/components/Masthead.tsx
@@ -0,0 +1,41 @@
+import * as S from './About.styles';
+
+export const Masthead = () => {
+  return (
+    <S.Container>
+      <S.Header>Masthead</S.Header>
+
+      <S.Text>
+        <strong>Angelica Johnson</strong> is a poet whose work has been featured
+        in numerous literary journals and anthologies. Her poetry often explores
+        themes of identity, race, and womanhood, and she has been recognized
+        with awards from the National Endowment for the Arts and the Poetry
+        Foundation. Angelica holds an MFA in Creative Writing from Columbia
+        University and currently teaches poetry workshops to incarcerated youth.
+      </S.Text>
+
+      <S.Text>
+        <strong>Mack Brown</strong> is a poet and essayist whose work has
+        appeared in publications such as The New Yorker, The Paris Review, and
+        The Atlantic. Their poetry collections include "The Weight of Snow" and
+        "The Silence That Follows." Mack's writing often grapples with questions
+        of mortality, memory, and love, and has been praised for its lyrical
+        precision and emotional depth. They hold a PhD in English from Yale
+        University and are a professor of creative writing at the University of
+        California, Berkeley.
+      </S.Text>
+
+      <S.Text>
+        <strong>Rudi Patel</strong> is an independent scholar of film and media
+        who has written extensively on topics such as representation,
+        globalization, and transnationalism in cinema. In addition to publishing
+        academic articles, Rudi also built and maintains the popular film
+        analysis website, FilmTheory.net. He holds a PhD in Film Studies from
+        the University of California, Los Angeles and has taught film history
+        and theory at several universities. Rudi is also a frequent speaker and
+        commentator on film and media issues in the media and at academic
+        conferences.
+      </S.Text>
+    </S.Container>
+  );
+};
