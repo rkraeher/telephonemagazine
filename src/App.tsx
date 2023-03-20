3d27569b9def033f24665e1d4fa3292b7019f7c4
Rudi Kraeher <rudi.kraeher@pipedrive.com>
2023-03-20 12:45:27 +0100

    move router into the App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css';
 import { Navbar } from 'components/Navbar/Navbar';
 import { Home } from 'components/Home/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ErrorPage } from 'components/ErrorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 export const App = () => {
   return (
@@ -9,7 +19,7 @@ export const App = () => {
       <header>
         <Navbar />
       </header>
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ErrorPage } from './components/ErrorPage/ErrorPage';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
